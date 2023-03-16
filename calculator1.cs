0cde6e820d9a2e511612d5be4afc957ca6b6fe83
dedenamikom <deden@students.amikom.ac.id>
2023-03-16 15:04:38 +0700

    menambahkan fungsi pengurangan

--- calculator1.cs
+++ calculator1.cs
@@ -2,7 +2,11 @@ class calculator
 {
 static void main(string[]args)
 {
+	int a = 10;
+	int b = 6;
 	
+	console.Writelineriteline("hasil penambahan: {0}+{1} = {2}",a,b, penambahan(a,b));
+	console.Writelineriteline("hasil pengurangan: {0}-{1} = {2}",a,b,pengurangan(a,b));
 }
 static int penambahan(int a, int b){
 	return a + b;
